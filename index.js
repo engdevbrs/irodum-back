1c466276a353155a72292076bb4ee0df5905bf0c
engdevbrs <fearzlx3@gmail.com>
2023-08-11 13:48:36 +1200

    cambio de paths servicios de imagenes

--- index.js
+++ index.js
@@ -405,9 +405,9 @@ app.post('/api/rating-worker',uploadComment.array('formFileMultiple',10),async (
         const ref = `${Date.now()}-${originalname}`;
         sharp(req.files[i].path)
             .resize({width: 400})
-            .toFile('/projects/commentsupload/' + ref)
+            .toFile('./projects/commentsupload/' + ref)
         let filesComment = {
-            filename: fs.readFileSync('/projects/commentsupload/' + ref),
+            filename: fs.readFileSync('./projects/commentsupload/' + ref),
             originalname: req.files[i].originalname
         }
         arrayFiles.push(filesComment)
