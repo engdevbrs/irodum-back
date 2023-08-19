9abbe3a96d59fa64e3eb2dcacf9a713211131ed8
engdevbrs <fearzlx3@gmail.com>
2023-08-19 15:36:51 +1200

    cambio a puerto 8080

--- index.js
+++ index.js
@@ -781,7 +781,7 @@ const cred = {
     key
 }
 
-app.listen(8443, () =>console.log("secure server running on port 443"))
+app.listen(8080, () =>console.log("secure server running"))
 
 const httpServer = https.createServer(cred,app)
-httpServer.listen(8443)
+httpServer.listen(8080)
